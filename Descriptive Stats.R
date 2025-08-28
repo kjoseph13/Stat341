0a313b7913c7188e93a3d1e3a7773a2bcf487e28
Kemauri Shyne Joseph <kjoseph@claflin.edu>
2025-08-28 16:22:37 -0400

    Add files via upload

---
+++ Descriptive Stats.R
@@ -0,0 +1,20 @@
+#Author: Kemauri: Date Aug 28, 2025; Purpose: Measures of Descriptive stats
+
+# Calculate mean/average for data
+
+# Generate dummy/sample data set
+data<- c(1:50)
+
+# Calculate mean/average for data
+mean(data)
+
+# Calculate Median for data
+median(data)
+
+#Calculation  Standard deviation
+sd(data)
+
+
+
+
+data2<- c(101:200)
