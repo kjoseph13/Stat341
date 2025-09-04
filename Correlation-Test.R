78fd5f535d1616203d983666c28856c6b39a1f52
Kemauri Shyne Joseph <kjoseph@claflin.edu>
2025-09-04 16:21:20 -0400

    Add files via upload

---
+++ Correlation-Test.R
@@ -0,0 +1,6 @@
+#Author: Pawar, Date:09-4-25, Purpose: Test the correlation
+
+ if(!require(devtools)) install.packages("devtools") ;devtools::install_github("kassambara/ggpubr")
+
+#Load the library ggpubr
+library(ggpubr)
